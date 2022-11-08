b9ce8b586be1059d7d0cddf197f5c5bd9c94cbae
Fernando Cuadros <daniel.cuadros@teamsoft.com.pe>
2022-11-08 18:08:48 -0500

    Primer commit node-express-backend 8-11-2022

--- src/config/monitor.config.js
+++ src/config/monitor.config.js
@@ -14,11 +14,11 @@ function buildConfiguration() {
     }    
 
     config.mysql = {
-        host: process.env.MYSQL_HOST,
-        username: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASS,
-        port: process.env.MYSQL_PORT,
-        database: process.env.MYSQL_DATABASE
+        host: process.env.MYSQLHOST,
+        username: process.env.MYSQLUSER,
+        password: process.env.MYSQLPASS,
+        port: process.env.MYSQLPORT,
+        database: process.env.MYSQLDATABASE
     }
 
     logger.info('Configuracion cargada exitosamente');
