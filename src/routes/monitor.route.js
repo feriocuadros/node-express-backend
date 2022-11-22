374fea55f7d778dca6f2a4179d8966e06a5a8971
Fernando Cuadros <daniel.cuadros@teamsoft.com.pe>
2022-11-22 08:50:21 -0500

    Actualizacion de fuentes 22-11-2022

--- src/routes/monitor.route.js
+++ src/routes/monitor.route.js
@@ -3,7 +3,7 @@ const monitorService = require('../services/monitor.services');
 const router = express.Router();
 const {logger} = require('../util/logger');
 
-router.get('/testnodeserver', async function (req, res, next) {
+router.get('/testnodeserver/', async function (req, res, next) {
     const response = await monitorService.getTestingConnection();
     return res.status(200).send(response);
 });
